0a11791bc4c631ffc183fe5354e962885b316505
Paul Conroy <paul@daft.ie>
2012-06-05 22:48:59 +0100

    Thick of it placeholders

---
+++ content/thickofit.js
@@ -0,0 +1,19 @@
+var thickofit = [
+    {
+        name:       "Malcolm Tucker"
+        , image:    "http://images2.wikia.nocookie.net/__cb52637/muppet/"
+            + "images/thumb/f/fc/Character.snuffy.jpg/"
+            + "130px-0%2C479%2C9%2C433-Character.snuffy.jpg"
+        , link:     "http://muppet.wikia.com/wiki/Mr._Snuffleupagus"
+        , quotes:   [
+            "Don't worry, Bird. We can hug for real when you get back to Sesame "
+                + "Street."
+            , "You could imagine that we're hugging, Bird."
+            , "I wouldn't know, Bird. The only temple I've ever seen is Shirley "
+                + "Temple"
+            , "Now that we know how Santa gets *into* the chimney, how does he "
+                + "get *out*?"
+            , "That is because you are imagining me, Bird."
+        ]
+    }
+];
